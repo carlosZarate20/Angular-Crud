445844ee758e95d9889a05ac62f8eb2599fe8a20
carlos.zarate <zaratec31@hotmail.com>
2018-05-15 17:01:26 -0500

    cambio de vista Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { Title } from '@angular/platform-browser';
+import { NgForm } from '@angular/forms';
 import { Router, Route } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -11,11 +10,8 @@ import { Router, Route } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup;
 
   constructor(
-    private fb: FormBuilder,
-    title: Title,
     private router: Router
   ) {
     /*title.setTitle('Login Angular 5');
@@ -31,15 +27,15 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   login(form: NgForm) {
-
-    // tslint:disable-next-line:triple-equals
-    if (form.value.email == 'admin@min.com' && form.value.password == 'admin123') {
-        this.router.navigate(['dashboard']);
+    console.log(form.value);
+    if (form.value.email === 'admin@admin.com' && form.value.password === 'admin123') {
+      localStorage.setItem('email', form.value.email);
+      this.router.navigate(['/dashboard']);
     } else {
       alert('Invalid credentials.');
     }
   }
-  loginUser(e) {
+  /*loginUser(e) {
     e.preventDefault();
     const email = e.target.elements[0].value;
     const password = e.target.elements[1].value;
@@ -52,7 +48,7 @@ export class LoginComponent implements OnInit {
     }
     /*const email = this.loginForm.get('email').value;
     const password = this.loginForm.get('password').value;
-    this.router.navigate(['/user']) ;*/
-  }
+    this.router.navigate(['/user']) ;
+  }*/
 
 }
